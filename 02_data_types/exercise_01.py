cfe92ea0251bee693d6fce53b5865296e8e93b08
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-25 16:15:30 -0700

    submit completion of Exercise 1

---
+++ 02_data_types/exercise_01.py
@@ -0,0 +1,13 @@
+# Identify the data type or class for each of the following values:
+
+# Value
+'True'          # string
+False           # boolean
+(1, 2, 3)       # tuple
+1.5             # float
+[1, 2, 3]       # list
+2               # integer
+range(5)        # range
+{1, 2, 3}       # set
+None            # NoneType
+{'foo': 'bar'}  # dictionary

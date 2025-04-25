0781211cd6d8284450327afe1035ab33e47f66c9
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-25 16:55:47 -0700

    add Exercise 2 (I couldn't figure it out)

---
+++ 03_basic_operations/exercise_02.py
@@ -0,0 +1,3 @@
+# Use the REPL and the arithmetic operators to extract the individual digits of 4936:
+
+After reading the hints and asking LSBot to clarify what the exercise is asking for, I still couldn't understand what it's asking, so I opted to skip this exercise.

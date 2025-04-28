48a1fcd42def4d80138d4da7943915c4261f6091
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 15:44:12 -0700

    include my answer for Exercise 13

---
+++ 06_functions_and_methods/exercise_13.py
@@ -0,0 +1,10 @@
+# Without running the following code, what do you think it will do?
+
+# def foo(first, second=3, third):
+#     print(first)
+#     print(second)
+#     print(third)
+
+# foo(42)
+
+I think this code will raise an error because having a default parameter followed by a non-default parameter isn't allowed.

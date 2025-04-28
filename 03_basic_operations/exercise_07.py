72677fa19210171ff43c4de895f09a8a3fb8bd63
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 09:55:53 -0700

    upload my corrected answer for Exercise 7

--- 03_basic_operations/exercise_07.py
+++ 03_basic_operations/exercise_07.py
@@ -3,3 +3,7 @@
 # int('3.1415')
 
 It will result in 3. The int typecast function loses the values after the decimal, effectively rounding down to the nearest whole number.
+
+# Correct answer:
+
+It will result in an error because the float in the argument is a string literal, but it must be an integer for int() to work.

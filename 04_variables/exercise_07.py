96c14cb943818ae526ce2053be7cde2c3ee8692a
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 10:40:48 -0700

    upload my answer for Exercise 7

---
+++ 04_variables/exercise_07.py
@@ -0,0 +1,13 @@
+# What happens when you run the following code? Why?
+
+# NAME = 'Victor'
+# print('Good Morning, ' + NAME)
+# print('Good Afternoon, ' + NAME)
+# print('Good Evening, ' + NAME)
+
+# NAME = 'Nina'
+# print('Good Morning, ' + NAME)
+# print('Good Afternoon, ' + NAME)
+# print('Good Evening, ' + NAME)
+
+It will run without problems, but the variable should be a constant, so it shouldn't be reassigned with a different value.

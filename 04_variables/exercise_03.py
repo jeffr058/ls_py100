d97af0a3131f397bebf1fda8f1e067c6ce110750
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 10:12:53 -0700

    upload my answer for Exercise 3

---
+++ 04_variables/exercise_03.py
@@ -0,0 +1,11 @@
+# Classify the following potential constant names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic and illegal names, explain your choice.
+
+# Name
+index           # non-idiomatic (should be all uppercase)
+CatName         # non-idiomatic (shouldn't be CamelCase)
+snake_case      # non-idiomatic (should be all uppercase)
+LAZY_DOG3       # idiomatic
+1ST             # illegal (can't start with a number)
+operand2        # non-idiomatic (should be all uppercase)
+BIG_NUMBER      # idiomatic
+π               # non-idiomatic (non-ASCII character)

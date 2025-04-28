401339535534c30653b767c0a4088ac15cce7c73
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 10:17:43 -0700

    upload my answer for Exercise 4

---
+++ 04_variables/exercise_04.py
@@ -0,0 +1,10 @@
+# Classify the following potential class names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic and illegal names, explain your choice.
+
+# Name
+index           # non-idiomatic (should be CamelCase)
+CatName         # idiomatic
+Lazy_Dog        # non-idiomatic (should be CamelCase without underscore)
+1ST             # illegal (can't start with a number or be all uppercase)
+operand2        # non-idiomatic (should be CamelCase)
+BigNumber3      # idiomatic
+πi              # non-idiomatic (shouldn't use non-ASCII character)

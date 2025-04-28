1afca52dcf078d2390d16b4b4eba68d96fd61473
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 15:41:15 -0700

    include my answer for Exercise 12

---
+++ 06_functions_and_methods/exercise_12.py
@@ -0,0 +1,10 @@
+# Without running the following code, what do you think it will do?
+
+# def foo(first, second=3, third=2):
+#     print(first)
+#     print(second)
+#     print(third)
+
+# foo()
+
+I think the code will raise an error because it expects at least one argument and no more than three arguments, and there is none provided.

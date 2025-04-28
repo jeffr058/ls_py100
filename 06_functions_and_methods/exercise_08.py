cf7c0963a00f90ea3a33eed67dce24b89f57cf29
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 15:32:56 -0700

    include my answer for Exercise 8

---
+++ 06_functions_and_methods/exercise_08.py
@@ -0,0 +1,9 @@
+# Without running the following code, what do you think it will do?
+
+# def foo(bar, qux):
+#     print(bar)
+#     print(qux)
+
+# foo(42, 3.141592, 2.718)
+
+I think the code will raise an error because it expects two arguments.

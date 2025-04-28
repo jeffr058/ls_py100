c97d53dc4d3e1cbec9dca6c12989fb75a19faed8
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 09:53:06 -0700

    upload my answer for Exercise 7

---
+++ 03_basic_operations/exercise_07.py
@@ -0,0 +1,5 @@
+# What will the following code do? Why?
+
+# int('3.1415')
+
+It will result in 3. The int typecast function loses the values after the decimal, effectively rounding down to the nearest whole number.

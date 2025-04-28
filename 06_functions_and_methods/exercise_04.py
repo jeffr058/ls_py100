6c1283c77a0814261bdbff9ca885446f7ed62eac
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 14:59:45 -0700

    upload my answer for Exercise 4

---
+++ 06_functions_and_methods/exercise_04.py
@@ -0,0 +1,19 @@
+# Consider this code:
+
+# def multiply_numbers(num1, num2, num3):
+#     result = num1 * num2 * num3
+#     return result
+
+# product = multiply_numbers(2, 3, 4)
+
+# Identify the following items in that code:
+
+# Item
+function name           # multiply_numbers
+function arguments      # 2, 3, 4
+function definition     # def
+function body           # Lines 2 & 3
+function parameters     # num1, num2, num3
+function invocation     # Line 5
+function return value   # Line 3
+all identifiers         # multiply_numbers, result, product

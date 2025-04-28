3788b4d327c7ee559c7ea38f772251be0c60a188
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 15:35:19 -0700

    include my answer for Exercise 9

---
+++ 06_functions_and_methods/exercise_09.py
@@ -0,0 +1,10 @@
+# Without running the following code, what do you think it will do?
+
+# def foo(first, second=3, third=2):
+#     print(first)
+#     print(second)
+#     print(third)
+
+# foo(42, 3.141592, 2.718)
+
+I think it will run 42, 3.141592, 2.718 because the second and third parameters are default parameters that will be used if no arguments for those parameters are given.

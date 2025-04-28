9f6de0e7581ae098e6bd43866c9f4c5d31c0d0db
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 16:25:18 -0700

    include my answer with corrections for Exercise 18

--- 06_functions_and_methods/exercise_18.py
+++ 06_functions_and_methods/exercise_18.py
@@ -13,3 +13,5 @@
 # Note: when working with integers, a value of 0 is "falsy"; all other integers are "truthy".
 
 Based on the provided function, I think numbers_1, numbers_2, and numbers_3 contains at least one number that is not evenly divisible by 3. The list numbers_4 doesn't have any numbers.
+
+Correction: numbers_1 and numbers_2 contain at least one number not evenly divisible by 3 (True is the expected answer from this exercise). The list numbers_3 is False as 0 % 3 results in 0. The list numbers_4 is also False because it does NOT contain any number that is not evenly divisible by 3.

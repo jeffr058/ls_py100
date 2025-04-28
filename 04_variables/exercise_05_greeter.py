e863c7158a8e12ab7d9fa10dcfa0ae1b38b9d39e
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 10:23:35 -0700

    upload my answer for Exercise 5

---
+++ 04_variables/exercise_05_greeter.py
@@ -0,0 +1,11 @@
+# Write a program named greeter.py that greets 'Victor' three times. Your program should use a variable and not hard code the string value 'Victor' in each greeting. Here's an example run of the program:
+
+# $ python greeter.py
+# Good Morning, Victor.
+# Good Afternoon, Victor.
+# Good Evening, Victor.
+
+name = 'Victor'
+print('Good Morning, ' + name)
+print('Good Afternoon, ' + name)
+print('Good Evening, ' + name)

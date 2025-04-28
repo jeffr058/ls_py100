2b60b006420179e90f334413b3ca7234f9f001bc
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 10:09:59 -0700

    upload my answer for Exercise 2

---
+++ 04_variables/exercise_02.py
@@ -0,0 +1,13 @@
+# Classify the following potential function names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic and illegal names, explain your choice.
+
+# Name
+index           # idiomatic
+CatName         # non-idiomatic (shouldn't be CamelCase)
+lazy_dog        # idiomatic
+quick_Fox       # non-idiomatic (should be all lowercase)
+1stCharacter    # illegal (can't start with a number)
+operand2        # idiomatic
+BIG_NUMBER      # non-idiomatic (should be all lowercase)
+π               # non-idiomatic (non-ASCII character)
+
+Answers are the same as for Exercise 1 as function naming convention is the same.

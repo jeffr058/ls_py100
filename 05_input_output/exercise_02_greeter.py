9b068dc0404fd3889e38ed01987cadb49b21e379
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 11:32:01 -0700

    correct single expression to two separate expressions

--- 05_input_output/exercise_02_greeter.py
+++ 05_input_output/exercise_02_greeter.py
@@ -8,4 +8,4 @@
 first_name = input('What is your first name? ')
 last_name = input('What is your last name? ')
 
-print(f'Hello, {first_name + ' ' + last_name}!')
+print(f'Hello, {first_name} {last_name}!')

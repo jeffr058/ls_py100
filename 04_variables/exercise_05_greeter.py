b4d7b75b86768597ee22312e51e973f08d736cb3
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 10:25:16 -0700

    add a period to end of each printed string

--- 04_variables/exercise_05_greeter.py
+++ 04_variables/exercise_05_greeter.py
@@ -6,6 +6,6 @@
 # Good Evening, Victor.
 
 name = 'Victor'
-print('Good Morning, ' + name)
-print('Good Afternoon, ' + name)
-print('Good Evening, ' + name)
+print('Good Morning, ' + name + '.')
+print('Good Afternoon, ' + name + '.')
+print('Good Evening, ' + name + '.')

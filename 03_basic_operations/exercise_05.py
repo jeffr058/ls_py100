0569f94934cb2ead6e95373fcbf9ddea875ba35e
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 09:45:44 -0700

    upload my answer for Exercise 5

---
+++ 03_basic_operations/exercise_05.py
@@ -0,0 +1,3 @@
+# Will an error occur if you try to access a list element with an index greater than or equal to the list's length? For example:
+
+Yes, because it is an attempt to access a list element at an index that doesn't exist at the moment.

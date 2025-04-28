0357f869c5daa7c1f773ee641fa4cad971cd7e9c
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 15:28:24 -0700

    include my answer for Exercise 7

---
+++ 06_functions_and_methods/exercise_07.py
@@ -0,0 +1,9 @@
+# Without running the following code, what do you think it will do?
+
+# def foo(bar, qux):
+#     print(bar)
+#     print(qux)
+
+# foo('Hello')
+
+I think it will result in an error because the interpreter expects foo() to receive two arguments.

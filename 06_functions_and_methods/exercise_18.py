109d2d0856194c3b7b6ae032f6703ef0d438b3c1
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 16:20:17 -0700

    include my answer for Exercise 18

---
+++ 06_functions_and_methods/exercise_18.py
@@ -0,0 +1,15 @@
+# The following function returns a list of the remainders of dividing the numbers in numbers by 3:
+
+# def remainders_3(numbers):
+#     return [number % 3 for number in numbers]
+
+# Use this function to determine which of the following lists contains at least one number that is not evenly divisible by 3 (that is, the remainder is not 0):
+
+# numbers_1 = [0, 1, 2, 3, 4, 5, 6]
+# numbers_2 = [1, 2, 4, 5]
+# numbers_3 = [0, 3, 6]
+# numbers_4 = []
+
+# Note: when working with integers, a value of 0 is "falsy"; all other integers are "truthy".
+
+Based on the provided function, I think numbers_1, numbers_2, and numbers_3 contains at least one number that is not evenly divisible by 3. The list numbers_4 doesn't have any numbers.

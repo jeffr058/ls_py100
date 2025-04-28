a79e1491e530c2883fd2db8071b645f5281d0395
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 14:46:09 -0700

    reflect attempt to improve code using single responsibility principle

--- 06_functions_and_methods/exercise_03_multiply.py
+++ 06_functions_and_methods/exercise_03_multiply.py
@@ -5,10 +5,16 @@
 # Enter the second number: 2.7183
 # 3.1416 * 2.7183 = 8.53981128
 
-def multiply():
-    a = input('Enter the first number: ')
-    b = input('Enter the second number: ')
-    answer = float(a) * float(b)
-    print(f'{a} * {b} = {answer}')
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
+ 
+def get_num(prompt):
+    num = input(prompt)
+    return float(num)
+    
+input_num1 = get_num('Enter the first number: ')
+input_num2 = get_num('Enter the second number: ')
+product = multiply(input_num1, input_num2)
+print(f'{input_num1} * {input_num2} = {product}')
 
-multiply()

137f4032a50ab35b6defb194f28974d0ae8949d5
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 13:34:51 -0700

    upload my answer for Exercise 1

---
+++ 06_functions_and_methods/exercise_01.py
@@ -0,0 +1,9 @@
+# What happens when you run the following program? Why do we get that result?
+
+# def set_foo():
+#     foo = 'bar'
+
+# set_foo()
+# print(foo)
+
+You get an error because foo was defined within the scope of the function.

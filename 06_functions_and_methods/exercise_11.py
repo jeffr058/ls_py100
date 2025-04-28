6719c28336c2d6374977f82f31bf88b390a0b7fa
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 15:39:25 -0700

    include my answer for Exercise 11

---
+++ 06_functions_and_methods/exercise_11.py
@@ -0,0 +1,14 @@
+# Without running the following code, what do you think it will do?
+
+# def foo(first, second=3, third=2):
+#     print(first)
+#     print(second)
+#     print(third)
+
+# foo(42)
+
+42
+3
+2
+
+The omission of the second and third arguments causes the default values of the second and third parameters to be used.

9188d1ca02e8e1ad42d28515628b09fd6ae54b26
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 09:57:56 -0700

    upload my answer for Exercise 8

---
+++ 03_basic_operations/exercise_08.py
@@ -0,0 +1,5 @@
+# To what value does the following expression evaluate?
+
+# '12' < '9'
+
+True, because these numbers are strings and the '9' is of greater value than the '1'. It doesn't need to evaluate the second digit of the string on the left.

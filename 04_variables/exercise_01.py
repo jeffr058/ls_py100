f0eae505ca2995cc3c6a9cd4304c950578b22d95
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-28 10:03:21 -0700

    upload my answer for Exercise 1

---
+++ 04_variables/exercise_01.py
@@ -0,0 +1,11 @@
+# Classify the following potential non-constant variable names as idiomatic, non-idiomatic, or illegal. For the non-idiomatic and illegal names, explain your choice.
+
+# Name
+index           # idiomatic
+CatName         # non-idiomatic
+lazy_dog        # idiomatic
+quick_Fox       # non-idiomatic
+1stCharacter    # illegal
+operand2        # idiomatic
+BIG_NUMBER      # non-idiomatic
+π               # non-idiomatic

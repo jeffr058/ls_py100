1d45f76d01f3113915c015a861bbf066eec29094
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-30 13:43:03 -0700

    include my attempt for Exercise 2

---
+++ 07_flow_control/exercise_02.py
@@ -0,0 +1,15 @@
+def even_or_odd(num):
+    if num % 2 == 0:
+        print('even')
+    else:
+        print('odd')
+
+even_or_odd(1)
+even_or_odd(2)
+even_or_odd(3)
+even_or_odd(4)
+even_or_odd(5)
+even_or_odd(10)
+even_or_odd(39)
+even_or_odd(1000)
+even_or_odd(5007)

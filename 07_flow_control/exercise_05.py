6ca3195107797acacfcac38f058e3ff9721a5f56
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-04-30 14:08:49 -0700

    include my answer for Exercise 5

---
+++ 07_flow_control/exercise_05.py
@@ -0,0 +1,11 @@
+# What does this code output, and why?
+
+# def is_list_empty(my_list):
+#     if my_list:
+#         print('Not Empty')
+#     else:
+#         print('Empty')
+
+# is_list_empty([])
+
+I think the code will output 'Empty' because the argument is an empty list [], which is considered falsy.

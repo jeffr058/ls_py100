f46d1811b5351435f32f5a7f94009b4054a3b55b
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-01 10:46:20 -0700

    include my answer for Exercise 3

---
+++ 08_intro_to_collections/exercise_03.py
@@ -0,0 +1,11 @@
+# Identify at least 2 differences between lists and tuples. Identify at least 2 ways that lists and tuples are similar.
+
+Differences:
+1. Lists are mutable whereas tuples are immutable.
+2. Lists use [] whereas tuples use ().
+3. When creating a single object, a tuple requires a trailing comma.
+
+Similarities:
+1. They are both sequences.
+2. The order objects are added is preserved 
+3. Each object is indexed with a specific position.

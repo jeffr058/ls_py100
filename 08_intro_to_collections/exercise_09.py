a51fb2847592ccf0feb84345bf536167f8f246d6
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-01 11:23:17 -0700

    include my answer for Exercise 9

---
+++ 08_intro_to_collections/exercise_09.py
@@ -0,0 +1,15 @@
+# my_list = [1, 2, 3, [4, 5, 6]]
+# another_list = list(my_list)
+
+# Given the above code, answer the following questions and explain your answers:
+
+# 1. Are the lists assigned to my_list and another_list equal?
+# 2. Are the lists assigned to my_list and another_list the same object?
+# 3. Are the nested lists at index 3 of my_list and another_list equal?
+# 4. Are the nested lists at index 3 of my_list and another_list the same object?
+# Don't write any code for this exercise.
+
+1. Yes, the equal values of each list make the lists equal.
+2. The lists aren't the same object, but their elements point to the same objects in memory.
+3. Yes, the nested lists are equal in value.
+4. Yes, it is the same object.

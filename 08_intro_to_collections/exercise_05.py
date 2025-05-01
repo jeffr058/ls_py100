d93533b0f6f53de8881c31e56475764ac4b4b73d
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-01 10:56:15 -0700

    include my answer for Exercise 5

---
+++ 08_intro_to_collections/exercise_05.py
@@ -0,0 +1,3 @@
+# How do the set types differ from the sequence types?
+
+Sets are unordered, meaning their objects (usually) aren't listed in the order they are added and aren't indexed like objects in sequences are.

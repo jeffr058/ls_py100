4990c0a4e999c3bdb7bad9950eacc5bf58a970b8
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-09 20:12:02 -0700

    give my answer for Exercise 2

---
+++ 09_using_collections/exercise_02.py
@@ -0,0 +1,6 @@
+# Use slicing to write Python code to print a 6-character substring of 'Launch School' that begins with the first c.
+
+print('Launch School'[4:10])
+
+name_to_slice = 'Launch School'
+print(name_to_slice[4:10])

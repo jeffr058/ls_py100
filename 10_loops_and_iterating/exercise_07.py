962a03da3f68f90dba02a5bf93e8a4118e4375ba
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-12 16:16:47 -0700

    update my code based on solution for Exercise 7

--- 10_loops_and_iterating/exercise_07.py
+++ 10_loops_and_iterating/exercise_07.py
@@ -4,8 +4,7 @@ my_tuple = (1, 'a', '1', 3, [7], 3.1415,
             -4, None, {1, 2, 3}, False)
 
 def find_integers(my_tuple):
-    int_list = [ obj for obj in my_tuple if type(obj) is int ]
-    return int_list
+    return [ obj for obj in my_tuple if type(obj) is int ]
 
 integers = find_integers(my_tuple)
 print(integers)                    # [1, 3, -4]

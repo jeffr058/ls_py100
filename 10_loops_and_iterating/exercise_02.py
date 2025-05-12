6bf6c681dc72f8e3912c57826ec10dbf7ba9d6e5
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-12 14:18:39 -0700

    update my code for Exercise 2 based on solution

--- 10_loops_and_iterating/exercise_02.py
+++ 10_loops_and_iterating/exercise_02.py
@@ -1,10 +1,8 @@
 # Modify the age.py program you wrote in Exercise 3 of the Input/Output chapter. The updated code should use a for loop to display the future ages.
 
 age = int(input('How old are you? '))
-decade = 10
-
 print(f'You are {age} years old.')
+print()
 
-for i in range(1, 5):
-    years = decade * i
-    print(f'In {years} years, you will be {age + years} years old.')
+for i in range(10, 50, 10):
+    print(f'In {i} years, you will be {age + i} years old.')

35804715cc6887e1aaa32d6adb0095388c8f941f
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-12 13:39:35 -0700

    add my answer for Exercise 1

---
+++ 10_loops_and_iterating/exercise_01.py
@@ -0,0 +1,8 @@
+# The following code causes an infinite loop (a loop that never stops iterating). Why?
+
+# counter = 0
+
+# while counter < 5:
+#     print(counter)
+
+It will be an infinite loop because without anything causing the counter to change, it will always be 0 and < 5, so the loop will never stop running.

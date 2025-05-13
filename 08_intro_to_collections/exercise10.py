477cb365730ff20bd3e1cdc604aa2b8cfe2ecb54
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-13 12:14:59 -0700

    remove files named incorrectly

--- 08_intro_to_collections/exercise10.py
+++
@@ -1,7 +0,0 @@
-# Bob expects the following code to print the names in alphabetical order. Without running the code, can you say whether it will? Explain your answer.
-
-# names = { 'Chris', 'Clare', 'Karis', 'Karl',
-#           'Max', 'Nick', 'Victor' }
-# print(names)
-
-I don't think the names will print in alphabetical order because the names are enclosed by curly brackets, indicating that names is a set, and sets are unordered collections.

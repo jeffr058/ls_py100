221973b8bb3fa742b7d2efa2e8b2d4899394a795
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-13 14:03:37 -0700

    include corrections for my answer

--- lesson_01/assignment_06/11_variables_as_pointers/exercise_03.py
+++ lesson_01/assignment_06/11_variables_as_pointers/exercise_03.py
@@ -10,6 +10,12 @@
 # dict2['Monty Python'] = 'Holy Grail'
 # print(dict1['Monty Python'])
 
+# Incorrect answer:
 It will print 'Holy Grail' as the value.
 
 By passing dict1 to the dict constructor, dict2 points to a shallow copy. dict2 is a new container that copied the references to the objects in dict1 and not new objects, meaning any changes to the mutable objects will be reflected in both dict1 and dict2.
+
+# Correct answer: 
+It will print 'The Life of Brian'. 
+
+dict2 is a new dictionary and changing the value only mutates the new dictionary. Even though it's a shallow copy, the object is immutable, meaning it can't be mutated, so any changes made will result in a new object in memory, and dict1 will not be affected. 

982edfb500750270659dccb213983318a9614915
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-13 13:13:59 -0700

    revise my answer to include why for Exercise 2

--- lesson_01/assignment_06/11_variables_as_pointers/exercise_02.py
+++ lesson_01/assignment_06/11_variables_as_pointers/exercise_02.py
@@ -7,4 +7,4 @@
 
 # Don't worry about having an exact match for the output. The important part is to show something that accurately represents set2.
 
-set2 will print {42, 'Monty Python', ('a', 'b', 'c'), range(5, 10)}.
+set2 will print {42, 'Monty Python', ('a', 'b', 'c'), range(5, 10)} because assigning a variable to another variable doesn't create a new object but references the same object in memory.

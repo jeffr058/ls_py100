cf74452877928643cd649c6ace125438e0676af3
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-13 12:52:14 -0700

    reorganize directories

---
+++ lesson_01/assignment_06/10_loops_and_iterating/dict_comp.py
@@ -0,0 +1,4 @@
+squares = { f'{number}-squared': number * number
+           for number in range(1, 6) }
+
+print(squares)
---
+++ lesson_01/assignment_06/10_loops_and_iterating/dicts.py
@@ -0,0 +1,3 @@
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+for item in my_dict.items():
+    print(item)

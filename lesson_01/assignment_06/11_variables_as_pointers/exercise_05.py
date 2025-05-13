a86d4e4b47745b1ec31e888f5bb0f8c73209084f
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-13 14:23:46 -0700

    add additional tests for Exercise 5

--- lesson_01/assignment_06/11_variables_as_pointers/exercise_05.py
+++ lesson_01/assignment_06/11_variables_as_pointers/exercise_05.py
@@ -19,3 +19,13 @@ print(dict1['b']    is dict2['b'])
 print(dict1['b'][0] is dict2['b'][0])
 print(dict1['b'][1] is dict2['b'][1])
 
+# All of these should print True
+
+print(dict1['a'][0][0] is dict2['a'][0][0])
+print(dict1['a'][0][1] is dict2['a'][0][1])
+print(dict1['a'][1][0] is dict2['a'][1][0])
+print(dict1['a'][1][1] is dict2['a'][1][1])
+print(dict1['b'][0][0] is dict2['b'][0][0])
+print(dict1['b'][0][1] is dict2['b'][0][1])
+print(dict1['b'][1][0] is dict2['b'][1][0])
+print(dict1['b'][1][1] is dict2['b'][1][1])

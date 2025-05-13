68bbec3d60c6c1c1d0e2bb42bc09b73494d2f31c
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-13 12:56:57 -0700

    include my answer to Exercise 1

---
+++ lesson_01/assignment_06/11_variables_as_pointers/exercise_01.py
@@ -0,0 +1,8 @@
+# In your own words, explain the difference between these two expressions.
+
+# my_object1 == my_object2
+# my_object1 is my_object2
+
+The first expression checks for whether the values of my_object1 and my_object2 are equal.
+    
+The second expression checks if the objects in both are the same objects.

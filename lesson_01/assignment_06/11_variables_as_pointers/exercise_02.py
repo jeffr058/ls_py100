72b76de0c0d24d96632698b721a61c5b5099d472
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-13 13:09:46 -0700

    add my answer for Exercise 2

---
+++ lesson_01/assignment_06/11_variables_as_pointers/exercise_02.py
@@ -0,0 +1,10 @@
+# Without running this code, what will it print? Why?
+
+# set1 = {42, 'Monty Python', ('a', 'b', 'c')}
+# set2 = set1
+# set1.add(range(5, 10))
+# print(set2)
+
+# Don't worry about having an exact match for the output. The important part is to show something that accurately represents set2.
+
+set2 will print {42, 'Monty Python', ('a', 'b', 'c'), range(5, 10)}.

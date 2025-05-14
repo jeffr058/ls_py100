209fab08f386de829c7a7c737c466dbb4f2c851c
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-14 13:40:21 -0700

    add my answer

---
+++ lesson_01/assignment_08/01_reading_documentation_1/05_list_element_access.py
@@ -0,0 +1,7 @@
+# Locate the documentation for the list built-in object in Python Documentation.
+
+# How can we access the second element ('and') in the list ['fish', 'and', 'chips']?
+
+list_name[1]
+
+or ['fish', 'and', 'chips'][1]  # added after checking solution

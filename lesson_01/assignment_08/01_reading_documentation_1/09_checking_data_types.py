8331cafae419984caa423fa5bfc8922b62a29aa6
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-14 14:20:21 -0700

    add my answer

---
+++ lesson_01/assignment_08/01_reading_documentation_1/09_checking_data_types.py
@@ -0,0 +1,9 @@
+# Referring to the official Python documentation, how would you identify the data type of the following values?
+
+# 23.5
+# 'Call me Ishmael.'
+# False
+# 0
+# None
+
+Use the type function.

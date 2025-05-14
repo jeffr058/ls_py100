eab6d73fde0f99035f47b68a6d5f68b7bd1f808d
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-14 15:19:28 -0700

    add my solution

---
+++ lesson_01/assignment_08/02_reading_documentation_2/06_argument_signatures.py
@@ -0,0 +1,3 @@
+# How many arguments does the str.join method expect? What happens if you call it with fewer or more arguments?
+
+It expects one argument of an iterable. An error will be raised if fewer or more arguments are passed into it.

7fabb4de33e7d528bc1fe7b8e098abe6d9bcdae8
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-14 15:25:26 -0700

    add my solution

---
+++ lesson_01/assignment_08/02_reading_documentation_2/07_find_substring.py
@@ -0,0 +1,3 @@
+# Using the official Python documentation, can you determine how to check whether a string contains a specific substring?
+
+Yes, the Common Sequence Operations subsection under Sequence Types covers using the in and not in operations.

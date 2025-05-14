702fcb950bad0dcfc44649ae30dd7e91fc3f9684
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-14 13:15:54 -0700

    add my answer

---
+++ lesson_01/assignment_08/01_reading_documentation_1/02_lowercase.py
@@ -0,0 +1,5 @@
+# Determine whether Python has a method to lowercase a string, for example converting 'Aloha, World!' into 'aloha, world!.
+
+Python has a method to lowercase a string:
+
+str.lower()

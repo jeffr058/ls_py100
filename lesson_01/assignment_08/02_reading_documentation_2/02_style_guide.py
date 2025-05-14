cc6ed1e79eff4a15e931c7059d625d758179a9aa
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-14 14:44:16 -0700

    add my solution

---
+++ lesson_01/assignment_08/02_reading_documentation_2/02_style_guide.py
@@ -0,0 +1,9 @@
+# In the following code snippet, find all violations of the PEP8 style guide. Rewrite it so that it conforms with the guide.
+
+iceCreamDensity=10              # ice_cream_density = 10
+
+while iceCreamDensity>0 :       # while ice_cream_density > 0:
+    print('Drip...')            
+    iceCreamDensity-=1          #     ice_cream_density -= 1
+
+print('The ice cream melted.')

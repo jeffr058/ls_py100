f68c6a44d144c5da679811294dba5618a75fecd7
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-15 14:05:08 -0700

    add my solution

---
+++ lesson_01/assignment_08/04_conditionals/03_yes_no_part_2.py
@@ -0,0 +1,7 @@
+# Rewrite your code from the previous exercise to use a ternary expression.
+
+import random
+random_number = random.randint(0, 1)
+
+# My solution
+print('Yes!') if random_number else print('No.')

fa943ff8bf9dd3377c0d23f0b07c92008b56d269
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-15 13:22:02 -0700

    add my solution

---
+++ lesson_01/assignment_08/03_loops/07_continue.py
@@ -0,0 +1,13 @@
+# Take a moment to read the Python documentation on the continue statement.
+
+# Write a for loop that iterates over the elements of the list cities and prints the length of each string. If the element is None, use the continue statement to skip forward to the next iteration without printing anything.
+
+cities = ['Istanbul', 'Los Angeles', 'Tokyo', None,
+          'Vienna', None, 'London', 'Beijing', None]
+
+# My solution
+for city in cities:
+    if city == None:
+        continue
+    else:
+        print(len(city))

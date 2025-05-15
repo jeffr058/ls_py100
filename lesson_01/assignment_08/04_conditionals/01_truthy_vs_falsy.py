4ce06da0e2b4a751266890b88859e830114868a7
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-15 13:46:24 -0700

    add my solution

---
+++ lesson_01/assignment_08/04_conditionals/01_truthy_vs_falsy.py
@@ -0,0 +1,7 @@
+# Without looking at your notes or the official documentation, try to recall all values that count as falsy in Python.
+
+False
+All zeros
+Empty strings
+Empty collections
+Some custom types may be falsy

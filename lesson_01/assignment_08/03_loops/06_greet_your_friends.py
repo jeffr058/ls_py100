03ef76dd98f0c4693867bbd26d470d4f73ca150b
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-15 13:15:08 -0700

    add my solution

---
+++ lesson_01/assignment_08/03_loops/06_greet_your_friends.py
@@ -0,0 +1,12 @@
+# Your friends just showed up! Given the following list of names, use a for loop to greet each friend individually.
+
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+# Expected output
+# Hello, Sarah!
+# Hello, John!
+# Hello, Hannah!
+# Hello, Dave!
+
+for friend in friends:
+    print(f'Hello, {friend}!')

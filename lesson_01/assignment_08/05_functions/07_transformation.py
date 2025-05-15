82bf4a27f5e89e22caa54e6baaac54ed6acc7b7c
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-15 15:41:54 -0700

    add my solution

--- lesson_01/assignment_08/05_functions/07_transformation.py
+++ lesson_01/assignment_08/05_functions/07_transformation.py
@@ -5,3 +5,12 @@ str2 = str1.replace('Ruby', 'Python')
 
 print(str1)
 print(str2)
+
+# practice with other two methods from LS solution
+first_word = 'Captain Ruby'[:8]
+second_word = 'Python'
+new_str = first_word + second_word
+
+word_list = 'Captain Ruby'.split()
+first = word_list[0]
+new_str1 = first + " " + second_word

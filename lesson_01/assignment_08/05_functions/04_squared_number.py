e5c011e324fd83bd9a82d9b18cbdc526c88c5eeb
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-15 15:00:22 -0700

    add my solution

---
+++ lesson_01/assignment_08/05_functions/04_squared_number.py
@@ -0,0 +1,10 @@
+# Write a function that accepts a single argument, a number, and returns the result of multiplying its argument by an exponent of 2 (also known as squaring the number or raising the number to the power of 2).
+
+# My solution
+def squared_number(num):
+    return num ** 2
+
+print(squared_number(3))
+# End of my solution
+
+squared_number(3)   # 9

30ab33fa9a58d112666a0d76868c15f8d54a4514
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-15 15:32:34 -0700

    add my solution

---
+++ lesson_01/assignment_08/05_functions/07_transformation.py
@@ -0,0 +1,7 @@
+# Use Python's string methods on the string 'Captain Ruby' to create a string with the value 'Captain Python'.
+
+str1 = 'Captain Ruby'
+str2 = str1.replace('Ruby', 'Python')
+
+print(str1)
+print(str2)

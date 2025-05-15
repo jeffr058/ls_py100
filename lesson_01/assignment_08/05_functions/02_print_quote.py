82bf0f9cf849b3758ca21570c5a27383f11101fb
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-15 14:53:28 -0700

    add my solution with revision from LS solution

---
+++ lesson_01/assignment_08/05_functions/02_print_quote.py
@@ -0,0 +1,10 @@
+# Write a function that prints Bruce Eckel's quote 'Python is executable pseudocode.'. What is the return value of the function?
+
+# My solution
+def bruce_eckel_quote():
+    print('Python is executable pseudocode.')  # removed return statement
+# End of my solution
+
+bruce_eckel_quote()
+
+# The return value is None  # added after reviewing solution

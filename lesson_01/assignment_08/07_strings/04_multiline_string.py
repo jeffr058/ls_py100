5d996a73bead1eacac5cc1171bb8adb73b5bc623
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-16 10:59:05 -0700

    add my solutions

---
+++ lesson_01/assignment_08/07_strings/04_multiline_string.py
@@ -0,0 +1,12 @@
+# How can you assign the following rhyme to a single variable while preserving the line break?
+
+# 1
+rhyme = '''A pirate I was meant to be!
+Trim the sails and roam the sea!'''
+
+print(rhyme)
+
+# 2
+rhyme = 'A pirate I was meant to be!\nTrim the sails and roam the sea!'
+
+print(rhyme)

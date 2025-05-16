5a15e48d18825f0e48c366a1a18b6ac7fb293e4a
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-16 09:14:44 -0700

    add my solution

---
+++ lesson_01/assignment_08/06_variable_scope/01_whats_my_value_part_1.py
@@ -0,0 +1,9 @@
+# What will the following code do and why? Don't run it until you have tried to answer.
+
+if True:
+    my_value = 20
+
+print(my_value)
+
+# my solution
+It will print 20 because new variables assigned in if statements are accessible in beyond its local scope.

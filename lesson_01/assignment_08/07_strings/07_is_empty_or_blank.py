317c11ed2fa96477aea5a0cc7e3fc878137836f6
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-16 11:49:28 -0700

    revise my code after review of LS solution

--- lesson_01/assignment_08/07_strings/07_is_empty_or_blank.py
+++ lesson_01/assignment_08/07_strings/07_is_empty_or_blank.py
@@ -1,8 +1,11 @@
 # Write an is_empty_or_blank function to determine whether a string is either empty or consists entirely of spaces. For example:
 
 def is_empty_or_blank(string):
-    return not string or ' ' in string
+    return not string.strip(' ')  # LS solution
 
 print(is_empty_or_blank('mars'))  # False
 print(is_empty_or_blank('  '))    # True
 print(is_empty_or_blank(''))      # True
+
+# My initial solution below didn't take into account whether the whole string consists entirely of spaces:
+# return not string or ' ' in string

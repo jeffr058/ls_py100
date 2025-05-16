eb53cbc0158290a51f6b3aa2b630dc0e7afb9508
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-16 09:42:35 -0700

    add my solution

---
+++ lesson_01/assignment_08/06_variable_scope/04_whats_my_value_part_4.py
@@ -0,0 +1,11 @@
+# What will the following code do and why? Don't run it until you have tried to answer.
+
+a = 1
+
+def my_function():
+    print(a)
+
+my_function()
+
+# My solution
+It will print a because the value of global variable can be read inside the function.

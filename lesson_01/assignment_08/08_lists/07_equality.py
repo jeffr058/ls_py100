a0eb4d6dc8c59144bae63f866c5d6878ab0d288b
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-16 14:42:55 -0700

    add my solution

---
+++ lesson_01/assignment_08/08_lists/07_equality.py
@@ -0,0 +1,10 @@
+# Predict the output of the code shown below. When you run the code, do you see what you expected? Why or why not?
+
+list1 = [2, 6, 4]
+list2 = [2, 6, 4]
+
+print(list1 == list2)
+
+# My solution
+# Initially I thought it would be True but after deciding it was a probably a trick question (self-inflicted mind games) I predicted the output will be False because the lists themselves are not equal in value, even if the values of their elements are.
+# When I ran the code, I didn't see False.

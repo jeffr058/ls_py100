2fff0d333f6618fea8aed6cdde17bab227e0ead1
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-16 11:28:54 -0700

    update my solution based on LS solution

--- lesson_01/assignment_08/07_strings/06_is_empty.py
+++ lesson_01/assignment_08/07_strings/06_is_empty.py
@@ -1,7 +1,8 @@
 # Write a function that checks whether a string is empty or not. For example:
 
 def is_empty(string):
-    return True if string == '' else False
+    # return True if string == '' else False  # My solution
+    return not string                         # Simpler LS solution
 
 print(is_empty('mars'))  # False
 print(is_empty('  '))    # False

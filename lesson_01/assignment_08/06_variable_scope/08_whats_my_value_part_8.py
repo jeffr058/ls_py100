65d416047f535fb2dbf89410cabe85a5f74b9103
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-16 10:08:41 -0700

    add my solution

---
+++ lesson_01/assignment_08/06_variable_scope/08_whats_my_value_part_8.py
@@ -0,0 +1,8 @@
+# What will the following code do and why? Don't run it until you have tried to answer.
+
+print(greeting)
+
+greeting = 'Hello world!'
+
+# My solution
+It will raise an error because in the global scope, the print function is called before the variable is initialized.

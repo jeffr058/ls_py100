10230719449e7ba878e067e39e70be870805ae6e
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-16 13:46:58 -0700

    revise my code after reviewing LS solution

--- lesson_01/assignment_08/07_strings/09_check_prefix.py
+++ lesson_01/assignment_08/07_strings/09_check_prefix.py
@@ -2,9 +2,12 @@
 # Write a function that checks whether a string starts with a specific prefix.
 
 def starts_with(string, prefix):
-    return string[:len(prefix)] == prefix
+    return string.startswith(prefix)
 
 # Examples
 print(starts_with("launch", "la"))   # True
 print(starts_with("school", "sah"))  # False
 print(starts_with("school", "sch"))  # True
+
+# My original solution before I found out there is a startswith method:
+# return string[:len(prefix)] == prefix

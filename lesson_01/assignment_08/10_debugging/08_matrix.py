3d27bb85e0d65303e68c374b49e511af5d48f4db
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-17 01:20:59 -0700

    add my solution

---
+++ lesson_01/assignment_08/10_debugging/08_matrix.py
@@ -0,0 +1,12 @@
+# We wanted to create a matrix 3x3 so we could use it to build a Tic-Tac-Toe game. However, we encountered an issue, as whenever we change a value in one nested list, all nested lists are affected. Can you fix the code?
+
+sub_list = ["-", "-", "-"]
+matrix = []
+
+for _ in range(3):
+    matrix[len(matrix):] = [["-", "-", "-"]]
+
+matrix[0][0] = "X"
+print(matrix) # [['X', '-', '-'], ['X', '-', '-'], ['X', '-', '-']]
+
+# Appending sub_list accesses it in memory, so any mutation by matrix or sub_list affects the other. Since matrix is appending the same sub_list three times, the change will show in all three.
